5b5c15b64a0a53387bfaf0866cf9ba51220bd591
Philip Bankier <pbankier@miso3.com>
2018-10-09 21:33:04 -0400

    initial

---
+++ src/pages/index.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+import Banner from '../components/Banner'
+
+import pic01 from '../assets/images/pic01.jpg'
+import pic02 from '../assets/images/pic02.jpg'
+import pic03 from '../assets/images/pic03.jpg'
+import pic04 from '../assets/images/pic04.jpg'
+import pic05 from '../assets/images/pic05.jpg'
+import pic06 from '../assets/images/pic06.jpg'
+
+class HomeIndex extends React.Component {
+    render() {
+
+        return (
+            <Layout>
+                <Helmet
+                    title="Gatsby Starter - Forty"
+                    meta={[
+                        { name: 'description', content: 'Sample' },
+                        { name: 'keywords', content: 'sample, something' },
+                    ]}
+                >
+                </Helmet>
+
+                <Banner />
+
+                <div id="main">
+                    <section id="one" className="tiles">
+                        <article style={{backgroundImage: `url(${pic01})`}}>
+                            <header className="major">
+                                <h3>Top Movies</h3>
+                                <p>The best movies available today</p>
+                            </header>
+                            <Link to="/topMovies" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic02})`}}>
+                            <header className="major">
+                                <h3>Top TV Shows </h3>
+                                <p>The best TV shows available today</p>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic03})`}}>
+                            <header className="major">
+                                <h3>Categories</h3>
+                                <p>See available categories</p>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic04})`}}>
+                            <header className="major">
+                                <h3>About this site</h3>
+                                <p>See what makes this the best movie/tv review site</p>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic05})`}}>
+                            <header className="major">
+                                <h3>Other projects</h3>
+                                <p>More projects created by PB Industries</p>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                    </section>
+                    <section id="two">
+                        <div className="inner">
+                            <header className="major">
+                                <h2>Reviews by the people, for the people</h2>
+                            </header>
+                            <p>The purpose of this site is to create a review site that utilizes the best features of the top reviews sites while leaving out the problems. Please let us know if you have any ideas or criticisms for us. </p>
+                        </div>
+                    </section>
+                </div>
+
+            </Layout>
+        )
+    }
+}
+
+export default HomeIndex
